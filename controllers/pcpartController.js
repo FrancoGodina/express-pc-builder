8f9e6bb7c379950f844eb3caf4bae1fe0c04cd80
franco <nahuelgodina@gmail.com>
2022-07-01 17:08:27 -0300

    Set up controllers

---
+++ controllers/pcpartController.js
@@ -0,0 +1,41 @@
+var Pcpart = require('../models/pcpart');
+
+// Display list of all Pcparts.
+exports.pcpart_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Pcpart list');
+};
+
+// Display detail page for a specific Pcpart.
+exports.pcpart_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Pcpart detail: ' + req.params.id);
+};
+
+// Display Pcpart create form on GET.
+exports.pcpart_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Pcpart create GET');
+};
+
+// Handle Pcpart create on POST.
+exports.pcpart_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Pcpart create POST');
+};
+
+// Display Pcpart delete form on GET.
+exports.pcpart_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Pcpart delete GET');
+};
+
+// Handle Pcpart delete on POST.
+exports.pcpart_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Pcpart delete POST');
+};
+
+// Display Pcpart update form on GET.
+exports.pcpart_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Pcpart update GET');
+};
+
+// Handle Pcpart update on POST.
+exports.pcpart_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Pcpart update POST');
+};
